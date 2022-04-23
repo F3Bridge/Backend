8f497e108dc85523a0a164f946f4cf8db350af5a
Piotr Adamczyk <xeno@xeno.yt>
2022-04-23 15:40:12 +0200

    Add refresh token mechanics

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { AuthTokensDto } from './dto/auth-tokens.dto';
 import { ChallengeRequestDto } from './dto/challenge-request.dto';
 import { ChallengeResponseDto } from './dto/challenge-response.dto';
 import { ChallengeDto } from './dto/challenge.dto';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller({ path: 'auth', version: '1' })
 export class AuthController {
@@ -21,5 +22,15 @@ export class AuthController {
   @HttpCode(200)
   verifyResponse(@Body() dto: ChallengeResponseDto): Promise<AuthTokensDto> {
     return this.authService.verifyResponse(dto);
+  }
+
+  @Post('refresh')
+  @Public()
+  @HttpCode(200)
+  async refreshToken(@Body() dto: RefreshTokenDto): Promise<AuthTokensDto> {
+    return {
+      refreshToken: dto.refreshToken,
+      accessToken: await this.authService.refresh(dto.refreshToken),
+    };
   }
 }
